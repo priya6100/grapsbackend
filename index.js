8298d10eff368dfb5ec68c359f7cb8e58725c005
priya6100 <priyaranjan@grapsnextsocial.com>
2021-10-31 16:46:54 +0530

    initall commitnew

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const CarrerEmail = require('./CarrerEmail')
 const NewsEmail = require('./NewsLetter')
 
 var app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const sessionId = uuid.v4();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.urlencoded({extended: false}));
